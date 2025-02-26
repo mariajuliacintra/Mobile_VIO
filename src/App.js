0b1fed91ea5a2abaa4eb0a799ae92ce69781fc4b
mariajuliacintra <mariajuliacintra31@gmail.com>
2025-02-26 12:50:53 -0300

    modificações de navegação de telas

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {NavigationContainer} from "@react-navigation/stack"; //IMPORTAÇÃO DE NAVEGAÇÃO
+import { createStackNavigator } from "@react-navigation/stack"; //IMPORTAÇÃO DE NAVEGAÇÃO
 import Login from "./screens/Login"
 import Cadastro from './screens/Cadastro';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-       <Cadastro/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: 'center',
-    backgroundColor:'#FEDAEC'
-  },
-  box1: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'red'
-  }, box2: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'pink'
-  }, box3:{
-    width: 50,
-    height: 50,
-    backgroundColor: '#AA3939'
-  },
-  box4:{
-    width: 50,
-    height: 50,
-    backgroundColor: '#850000'
-  }, 
-  row:{
-    flexDirection:'row'
-  },
-  text:{
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  input:{
-    borderWidth:1,
-    borderColor: 'gray',
-    width:'80%',
-    padding:10,
-    marginVertical:10,
-  }
-});
